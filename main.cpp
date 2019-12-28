0348c6bde64feaaa7283632627faf958bc7d348c
HaoJuChang <black1120rock@gmail.com>
2019-12-28 10:32:25 +0800

    hi

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct RS_STRUCT{
+    string RS = "";
+    bool busy = false;
+    
+}
+
+int main(){
+
+    return 0;
+}
